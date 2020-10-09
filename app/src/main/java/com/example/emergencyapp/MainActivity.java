2cc3624ac73df4a05b3a95ab138237468f720059
Ramihozi <Ramihozi@users.noreply.github.com>
2020-10-08 22:58:32 -0700

    Added Call_By_Intent and buttons now function when pressed to make the phone call. Next is adding GPS and about sections. Estimated time for completion ~10/9/20 or 10/10/20

--- app/src/main/java/com/example/emergencyapp/MainActivity.java
+++ app/src/main/java/com/example/emergencyapp/MainActivity.java
@@ -1,17 +1,27 @@
 package com.example.emergencyapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int REQUEST_CALL = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +35,30 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-    }
 
+
+
+
+
+        ImageButton callButton = findViewById(R.id.imageButton4);
+        callButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                makeCall();
+            }
+            
+        });
+    }
+    public void makeCall() {
+        Intent callIntent = new Intent( Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel:5416569727") );
+
+        if (ActivityCompat.checkSelfPermission( MainActivity.this,
+                Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{(Manifest.permission.CALL_PHONE)}, REQUEST_CALL);
+        } else {
+            String dial = "tel:5416569727";
+            startActivity( callIntent );
+        }
+    }
 }
